a1abf0649145b9b148ea83dc8c8d5a273bb77d24
Simon Carter <simon.c.carter@gmail.com>
2018-07-20 12:16:58 +0200

    js 2 ts

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
-import { rootEpic, rootReducer } from './modules/root'
+import { rootEpic, rootReducer } from 'redux/modules/root'
 
-/* eslint-disable-next-line */
-const NODE_ENV = process.env.NODE_ENV
+const isProd = process.env.NODE_ENV === 'production'
 
 const epicMiddleware = createEpicMiddleware(rootEpic)
 
 export default function configureStore() {
-  const composeEnhancers = NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  const composeEnhancers = !isProd && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
   const store = createStore(
     rootReducer,
