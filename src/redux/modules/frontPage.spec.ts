56cee4023d5ff8989fa52a4595bac5cd37486757
Simon Carter <simon.c.carter@gmail.com>
2018-09-22 19:18:26 +0100

    Update tests

--- src/redux/modules/frontPage.spec.ts
+++ src/redux/modules/frontPage.spec.ts
@@ -5,10 +5,11 @@ import { ActionsObservable } from 'redux-observable'
 import { COPY_DATA } from './data'
 import { onError, mockStore } from '../../utils'
 
-import frontPageReducer, { 
+import frontPageReducer, {
   textInDetails, initSliceEpic, initialState,
-  INIT_SLICE, FILTER, SET_FILTERED_CONFERENCES, 
-  SET_IS_ACTIVE, filterEpic, createConference
+  INIT_SLICE, FILTER, SET_FILTERED_CONFERENCES,
+  SET_IS_ACTIVE, filterEpic, createConference,
+  routingEpic
 } from './frontPage'
 
 describe('frontPage', () => {
@@ -19,7 +20,7 @@ describe('frontPage', () => {
       phrasesToSearchIn = ['aaa', 'aaab']
       query = 'b'
     }))
-    
+
     it('should return true', () => {
       // arrange 
       // act
@@ -41,7 +42,7 @@ describe('frontPage', () => {
     })
   })
 
-  describe('filterVideos', () => {})
+  describe('filterVideos', () => { })
 
   describe('createConference', () => {
     it('should return empty object given empty array of matched videos', () => {
@@ -60,11 +61,11 @@ describe('frontPage', () => {
       const videos = Object.freeze(['aaa', 'bbb'])
       const mVideos = ['ccc', 'ddd']
       const confIdx = 'idx'
-      
-      const conferences = Immutable<any>({'idx': {videos}})
+
+      const conferences = Immutable<any>({ 'idx': { videos } })
       const newConferences = {}
-      const expectedResult = conferences.setIn([confIdx], {videos: [...mVideos]})
-      
+      const expectedResult = conferences.setIn([confIdx], { videos: [...mVideos] })
+
       // act
       const result = createConference(conferences, 'idx', newConferences, mVideos)
 
@@ -73,11 +74,11 @@ describe('frontPage', () => {
     })
   })
 
-  describe('initSliceEpic' , () => {
+  describe('initSliceEpic', () => {
     it('should return the correct action', (done) => {
       // arrange
       const payload = { conferences: {} }
-      const action$ = ActionsObservable.of({type: COPY_DATA, payload})
+      const action$ = ActionsObservable.of({ type: COPY_DATA, payload })
       const expectedPayload = { conferences: {}, filteredConferences: {} }
 
       // act
@@ -99,14 +100,73 @@ describe('frontPage', () => {
     it('should return the correct action', (done) => {
       // arrange
       const payload = { conferences: {} }
-      const action$ = ActionsObservable.of({type: FILTER, payload})
-      const expectedPayload = {conferences: {}}
+      const action$ = ActionsObservable.of({ type: FILTER, payload })
+      const expectedPayload = { conferences: {} }
       // act
       filterEpic(action$, mockStore(), null)
         .subscribe(
           action => {
+            // assert
             expect(action.type).toEqual(INIT_SLICE)
             expect(action.payload).toEqual(expectedPayload)
+            done()
+          },
+          onError(done)
+        )
+    })
+  })
+
+  describe('routingEpic', () => {
+    it('should call return the correct action when a query string is present', (done) => {
+      // arrange
+      const payload = 'keynote'
+      const action$ = ActionsObservable.of({ type: FILTER, payload })
+
+      // CALL_HISTORY_METHOD is dispatched by connected-react-router,
+      // though this is not displayed in redux dev tools. The below action
+      // won't match what you see in your redux dev logger.
+      const expectedAction = {
+        "payload": {
+          "args": [`/search?query=${payload}`],
+          "method": "push"
+        }, 
+        "type": "@@router/CALL_HISTORY_METHOD"
+      }
+
+      // act
+      routingEpic(action$, mockStore(), null)
+        .subscribe(
+          action => {
+            // assert 
+            expect(action).toEqual(expectedAction)
+            done()
+          },
+          onError(done)
+        )
+    })
+
+    it('should call return the correct action when the query string is empty', (done) => {
+      // arrange
+      const payload = ''
+      const action$ = ActionsObservable.of({ type: FILTER, payload })
+
+      // CALL_HISTORY_METHOD is dispatched by connected-react-router,
+      // though this is not displayed in redux dev tools. The below action
+      // won't match what you see in your redux dev logger.
+      const expectedAction = {
+        "payload": {
+          "args": ['/'],
+          "method": "push"
+        }, 
+        "type": "@@router/CALL_HISTORY_METHOD"
+      }
+
+      // act
+      routingEpic(action$, mockStore(), null)
+        .subscribe(
+          action => {
+            // assert 
+            expect(action).toEqual(expectedAction)
             done()
           },
           onError(done)
@@ -117,17 +177,17 @@ describe('frontPage', () => {
   describe('reducer', () => {
     it('should return the initial state', () => {
       // act
-      const result = frontPageReducer(undefined, {type: 'EEE'})
+      const result = frontPageReducer(undefined, { type: 'EEE' })
       // assert
       expect(result).toEqual(initialState)
     })
 
     it('should initiate the slice with the payload', () => {
       // arrange 
-      const payload = {'conferences': {'idx': {'title': 'lalala'}}}
+      const payload = { 'conferences': { 'idx': { 'title': 'lalala' } } }
       const expectedResult = initialState.merge(payload)
       const action = { type: INIT_SLICE, payload }
-      
+
       // act
       const result = frontPageReducer(undefined, action)
 
@@ -139,7 +199,7 @@ describe('frontPage', () => {
       // arrange 
       const payload = 'filter value'
       const action = { type: FILTER, payload }
-      
+
       // act
       const result = frontPageReducer(undefined, action)
 
@@ -149,9 +209,9 @@ describe('frontPage', () => {
 
     it('should set the filtered conferences', () => {
       // arrange 
-      const payload = {'idx': 'filtered conferences'}
+      const payload = { 'idx': 'filtered conferences' }
       const action = { type: SET_FILTERED_CONFERENCES, payload }
-      
+
       // act
       const result = frontPageReducer(undefined, action)
 
@@ -163,13 +223,15 @@ describe('frontPage', () => {
       // arrange 
       const payload = false
       const action = { type: SET_IS_ACTIVE, payload }
-      
+
       // act
       const result = frontPageReducer(undefined, action)
 
       // assert
       expect(result.isActive).toBe(false)
     })
+
+
 
   })
 
