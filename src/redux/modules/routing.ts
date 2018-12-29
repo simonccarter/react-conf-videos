43387b9f7f793f8f08f17053c5646ca7e82713ec
Simon Carter <simon.c.carter@gmail.com>
2018-12-29 11:02:35 +0100

    Search for conf by name

--- src/redux/modules/routing.ts
+++ src/redux/modules/routing.ts
@@ -5,14 +5,16 @@ import { map, mergeMap } from 'rxjs/operators';
 import { push } from 'connected-react-router'
 import { Epic, combineEpics } from 'redux-observable'
 
+import { sluggifyUrl } from 'utils'
 import { isFilterEmpty } from 'utils'
 import { LOAD_DATA_END } from './data'
 import { searchActions } from './search'
 import { ApplicationState } from 'redux/modules'
 import { conferencePageActions } from './conferencePage'
+import { IndexedConferences } from '../../domain';
 
 export const NAVIGATE_TO_SEARCH_URL = 'routing/NAVIGATE_TO_SEARCH_URL'
-export const navigateToSearchURL = (payload: string) => ({type: NAVIGATE_TO_SEARCH_URL, payload})
+export const navigateToSearchURL = (payload: string) => ({ type: NAVIGATE_TO_SEARCH_URL, payload })
 
 export const isSearchPage = (pathname: string) => pathname === '/' || pathname.includes('/search')
 
@@ -23,10 +25,23 @@ export const extractQueryFromSearch = (search: string) => {
   return query
 }
 
-const getConferenceIdFromTitleFromPathname = (pathname: string) => {
+const getConferenceIdFromPathname = (pathname: string) => {
   const match = pathname.match(/conference\/(.*?)\//)
-  const id = match && match[1]
-  return id as string
+  const id = match && match[1] ? match[1] : ''
+  return id
+}
+
+const getConferenceNameFromPathname = (pathname: string) => {
+  const match = pathname.match(/conference\/.*?\/(.*?)$/)
+  const title = match && match[1] ? match[1] : ''
+  return title
+}
+
+const searchConferencesGivenUrlTitle = (title: string, conferences: IndexedConferences) => {
+  const matchedConferences = Object.keys(conferences).filter((confKey) => {
+    return sluggifyUrl(conferences[confKey].title) === title
+  })
+  return matchedConferences[0]
 }
 
 // load data in right place on initial page load
@@ -39,7 +54,14 @@ export const loadDataForRoute: Epic<any, any, ApplicationState> = (action$, stor
         if (isSearchPage(pathname)) {
           return of(searchActions.filter(extractQueryFromSearch(search)))
         } else {
-          const id = getConferenceIdFromTitleFromPathname(pathname)
+          let id = getConferenceIdFromPathname(pathname)
+          // check id exists; if doesn't try and find from name
+          if (!store.value.data.conferences[id]) {
+            // can we find id from name ?
+            const title = getConferenceNameFromPathname(pathname)
+            id = searchConferencesGivenUrlTitle(title, store.value.data.conferences)
+          }
+
           return concat([
             conferencePageActions.setConferenceDetails(id),
             searchActions.filter(extractQueryFromSearch(search))
