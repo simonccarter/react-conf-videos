6fc35ea2dbd2b38996812b0ff204aad982fd8cee
Simon Carter <simon.c.carter@gmail.com>
2019-05-02 18:41:38 +0200

    Fix ref in SearchInput

--- src/components/SearchInput/SearchInput.spec.tsx
+++ src/components/SearchInput/SearchInput.spec.tsx
@@ -1,127 +1,129 @@
-import { mount, shallow } from 'enzyme'
-import toJSON from 'enzyme-to-json'
-import * as React from 'react'
+import { mount, shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import * as React from 'react';
 
-import { blurRef, onKeyUpHandlers, SearchInput, SearchInputInner } from './SearchInput'
+import {
+  blurRef,
+  onKeyUpHandlers,
+  SearchInput,
+  SearchInputInner
+} from './SearchInput';
 
 describe('SearchInputInner', () => {
-
   const shallowComp = (mockFN = jest.fn(), placeholder?: string) => {
     return shallow(
       <SearchInputInner
         filterValue=""
         onChange={jest.fn()}
         onKeyUpHandlers={mockFN}
-        setRef={jest.fn()}
         myRef={null}
         blurRef={jest.fn()}
         placeholder={placeholder}
       />
-    )
-  }
+    );
+  };
 
   it('should render', () => {
     // arrange, act
-    const comp = shallowComp()
+    const comp = shallowComp();
     // assert
-    expect(toJSON(comp)).toMatchSnapshot()
-  })
+    expect(toJSON(comp)).toMatchSnapshot();
+  });
 
   it('should call onKeyUpHandlers on onKeyUp with keyCode 13', () => {
     // arrange
-    const mockFN = jest.fn()
-    const comp = shallowComp(mockFN)
+    const mockFN = jest.fn();
+    const comp = shallowComp(mockFN);
 
     // act
-    comp.find('.root').simulate('keyUp', {keyCode: 13})
+    comp.find('.root').simulate('keyUp', { keyCode: 13 });
 
     // assert
-    expect(mockFN).toHaveBeenCalled()
-  })
+    expect(mockFN).toHaveBeenCalled();
+  });
 
   it('should set placeholder if passed', () => {
     // arrange
-    const mockFN = jest.fn()
-    const placeholder = 'test placeholder'
+    const mockFN = jest.fn();
+    const placeholder = 'test placeholder';
 
     // act
-    const comp = shallowComp(mockFN, placeholder)
+    const comp = shallowComp(mockFN, placeholder);
 
     // assert
-    expect(toJSON(comp)).toMatchSnapshot()
-  })
-})
+    expect(toJSON(comp)).toMatchSnapshot();
+  });
+});
 
 describe('SearchInput', () => {
-
   it('should render and contain connected props', () => {
     // arrange
-    const mockFN = jest.fn()
-    const comp = <SearchInput onChange={mockFN} filterValue="" />
+    const mockFN = jest.fn();
+    const comp = <SearchInput onChange={mockFN} filterValue="" />;
 
     // act
-    const wrapper = shallow(comp)
+    const wrapper = shallow(comp);
 
     // assert
-    expect(toJSON(wrapper)).toMatchSnapshot()
-    expect(wrapper.props().filterValue).toEqual('')
-  })
+    expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper.props().filterValue).toEqual('');
+  });
 
   it('should dispatch correct actions on change handler call', () => {
     // arrange
-    const mockFN = jest.fn()
-    const value = 'search input'
-    const comp = <SearchInput onChange={mockFN} filterValue="" />
+    const mockFN = jest.fn();
+    const value = 'search input';
+    const comp = <SearchInput onChange={mockFN} filterValue="" />;
 
     // act
-    const wrapper = mount(comp)
-    wrapper.find('input').simulate('change', {target: { value }})
+    const wrapper = mount(comp);
+    wrapper.find('input').simulate('change', { target: { value } });
 
     // assert
-    expect(mockFN).toHaveBeenCalled()
-  })
-})
+    expect(mockFN).toHaveBeenCalled();
+  });
+});
 
 describe('blueRef', () => {
   it('should call the blurRef method of the element', () => {
     // arrange
-    const mockBlur = jest.fn()
-    const fakeElement = {myRef: {blur: mockBlur}}
+    const mockBlur = jest.fn();
+    const fakeElement = { myRef: { blur: mockBlur } };
 
     // act
-    blurRef(fakeElement)()
+    blurRef(fakeElement)();
 
     // assert
-    expect(mockBlur).toHaveBeenCalledTimes(1)
-  })
-})
+    expect(mockBlur).toHaveBeenCalledTimes(1);
+  });
+});
 
 describe('onKeyUpHandlers', () => {
   it('should call blurRef when keyCode is 13', () => {
     // arrange
     // tslint:disable-next-line
-    const blurRef = jest.fn()
-    const input = {blurRef, setRef: jest.fn()}
-    const mockEvent = {keyCode: 13} as React.KeyboardEvent<HTMLInputElement>
+    const blurRef = jest.fn();
+    const input = { blurRef, setRef: jest.fn() };
+    const mockEvent = { keyCode: 13 } as React.KeyboardEvent<HTMLInputElement>;
 
     // act
-    onKeyUpHandlers(input)(mockEvent)
+    onKeyUpHandlers(input)(mockEvent);
 
     // assert
-    expect(blurRef).toHaveBeenCalledTimes(1)
-  })
+    expect(blurRef).toHaveBeenCalledTimes(1);
+  });
 
   it('should not call blurRef when keyCode is not 13', () => {
     // arrange
     // tslint:disable-next-line
-    const blurRef = jest.fn()
-    const input = {blurRef, setRef: jest.fn()}
-    const mockEvent = {keyCode: 14} as React.KeyboardEvent<HTMLInputElement>
+    const blurRef = jest.fn();
+    const input = { blurRef, setRef: jest.fn() };
+    const mockEvent = { keyCode: 14 } as React.KeyboardEvent<HTMLInputElement>;
 
     // act
-    onKeyUpHandlers(input)(mockEvent)
+    onKeyUpHandlers(input)(mockEvent);
 
     // assert
-    expect(blurRef).toHaveBeenCalledTimes(0)
-  })
-})
+    expect(blurRef).toHaveBeenCalledTimes(0);
+  });
+});
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,60 +1,64 @@
 /* tslint:disable:no-shadowed-variable */
-import * as React from 'react'
-import { compose, pure, withHandlers, withStateHandlers } from 'recompose'
+import * as React from 'react';
+import { compose, pure, withHandlers, withStateHandlers } from 'recompose';
 
-import styles from './SearchInput.scss'
+import styles from './SearchInput.scss';
 
 type Props = {
-  filterValue: string,
-  placeholder?: string,
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-}
+  filterValue: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-type WithState = { myRef: any }
+type WithState = { myRef: any };
 
 type WithStateHandlers = {
-  setRef: () => WithState
-  blurRef: () => WithState
-}
+  blurRef: () => WithState;
+};
 
 type WithHandlers = {
-  onKeyUpHandlers: (e: React.KeyboardEvent<HTMLInputElement>) => void
-}
+  onKeyUpHandlers: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+};
 
-type CombinedProps = Props & WithState & WithStateHandlers & WithHandlers
+type CombinedProps = Props & WithState & WithStateHandlers & WithHandlers;
 
 export const SearchInputInner: React.SFC<CombinedProps> = ({
-  filterValue, onChange, onKeyUpHandlers, setRef, placeholder = 'Search'
+  filterValue,
+  onChange,
+  onKeyUpHandlers,
+  myRef,
+  placeholder = 'Search'
 }) => (
   <input
     type="text"
-    ref={setRef}
+    ref={myRef}
     value={filterValue}
     onChange={onChange}
     className={styles.root}
     placeholder={placeholder}
     onKeyUp={onKeyUpHandlers}
   />
-)
+);
 
 // for testing
-export const blurRef = ({myRef}: WithState) => () => myRef.blur()
-export const onKeyUpHandlers = ({blurRef}: WithStateHandlers) => (e: React.KeyboardEvent<HTMLElement>) => {
+export const blurRef = ({ myRef }: WithState) => () => myRef.blur();
+export const onKeyUpHandlers = ({ blurRef }: WithStateHandlers) => (
+  e: React.KeyboardEvent<HTMLElement>
+) => {
   if (e.keyCode === 13) {
-    blurRef()
+    blurRef();
   }
-}
+};
 
 export const SearchInput = compose<CombinedProps, Props>(
   pure,
   withStateHandlers<WithState, WithStateHandlers, Props>(
-    { myRef: null },
+    { myRef: React.createRef() },
     {
-      setRef: () => (ref) => ({ myRef: ref }),
       blurRef
     }
   ),
   withHandlers<WithState & WithStateHandlers, WithHandlers>({
     onKeyUpHandlers
   })
-)(SearchInputInner)
+)(SearchInputInner);
