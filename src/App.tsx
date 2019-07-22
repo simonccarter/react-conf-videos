ebe5443cdfd77125ade25c75dc8dceb40cf21827
Simon Carter <simon.c.carter@gmail.com>
2019-07-22 10:47:30 +0200

    Add debounce to analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,17 @@ import * as ReactGA from 'react-ga';
 import { hot } from 'react-hot-loader';
 import { Redirect, Route, Switch } from 'react-router';
 
-import { Tracker } from 'components';
 import { ConfPage, FrontPage } from 'components/Pages';
 
 import './index.scss';
 
 ReactGA.initialize('UA-111717324-1');
 
-const FrontPageTracked = Tracker(FrontPage);
-const ConfPageTracked = Tracker(ConfPage);
-
 const App = () => (
   <Switch>
     <Redirect from="/#/conference/:name" to="/conference/:name" />
-    <Route
-      path="/conference/*"
-      render={props => <ConfPageTracked {...props} />}
-    />
-    <Route path="/search" render={props => <FrontPageTracked {...props} />} />
+    <Route path="/conference/*" render={props => <ConfPage {...props} />} />
+    <Route path="/search" render={props => <FrontPage {...props} />} />
     <Route path="/" render={() => <Redirect to="/search" />} />
   </Switch>
 );
