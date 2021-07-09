c9d6d7313e209c54d58c812dbaaf019c84e615d9
Simon Carter <simon.c.carter@gmail.com>
2021-07-09 08:35:42 +0200

    WIP

--- src/components/Pages/ConfPage/ConfPage.tsx
+++ src/components/Pages/ConfPage/ConfPage.tsx
@@ -1,46 +1,46 @@
 import {
-    Header,
-    InnerLayoutContainer,
-    List,
-    Meta,
-    ResultDetails,
-    SearchInput
+  Header,
+  InnerLayoutContainer,
+  List,
+  Meta,
+  ResultDetails,
+  SearchInput,
 } from 'components';
-import useConferencePage from 'hooks/useConferencePage';
+import useSearch from 'hooks/useSearch';
 import * as React from 'react';
 
 export const ConfPage: React.FC<any> = () => {
-    const {
-        localQuery,
-        conference,
-        list,
-        onInputChange,
-        numberOfVideos,
-        numberOfConferences
-    } = useConferencePage("/conference/:name")
-    return (
-        <div>
-            <Meta title={conference?.title || ''} />
-            <Header
-                title={conference?.title || ''}
-                titleLink={conference?.website || ''}
-                tagline={`
+  const {
+    localQuery,
+    conference,
+    list,
+    onInputChange,
+    numberOfVideos,
+    numberOfConferences,
+  } = useSearch('/conference/:name');
+  return (
+    <div>
+      <Meta title={conference?.title || ''} />
+      <Header
+        title={conference?.title || ''}
+        titleLink={conference?.website || ''}
+        tagline={`
         ${conference?.date || ''} \-
         ${conference?.videos.length} \
-        ${conference?.videos?.length !== 1
-                        ? 'videos'
-                        : 'video'
-                    } `}
-            />
-            <InnerLayoutContainer>
-                <SearchInput
-                    onChange={onInputChange}
-                    filterValue={localQuery}
-                    placeholder={`Search ${conference?.title}`}
-                />
-                <ResultDetails numberOfVideos={numberOfVideos} numberOfConferences={numberOfConferences} />
-                <List conferences={list} />
-            </InnerLayoutContainer>
-        </div>
-    );
+        ${conference?.videos?.length !== 1 ? 'videos' : 'video'} `}
+      />
+      <InnerLayoutContainer>
+        <SearchInput
+          onChange={onInputChange}
+          filterValue={localQuery}
+          placeholder={`Search ${conference?.title}`}
+        />
+        <ResultDetails
+          numberOfVideos={numberOfVideos}
+          numberOfConferences={numberOfConferences}
+        />
+        <List conferences={list} />
+      </InnerLayoutContainer>
+    </div>
+  );
 };
--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,71 +1,96 @@
-import * as React from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { computedResultDetails, listState, queryState } from '../state'
-import { sluggifyUrl } from '../utils'
-import { useDebounce } from './useDebounce'
+import * as React from 'react';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { computedResultDetails, listState, queryState } from '../state';
+import { sluggifyUrl } from '../utils';
+import { useDebounce } from './useDebounce';
 
 import * as queryString from 'query-string';
-import { getList, search } from '../services/web'
+import { getList, search } from '../services/web';
+import { ConferenceTransformed } from 'domain/TransformedJSON';
 
 const useSearch = (routeMatch?: string) => {
-    const history = useHistory();
-    const location = useLocation()
-    
-    const [query, setQuery] = useRecoilState(queryState)
-    const [localQuery, setLocalQuery] = React.useState(query)
-    const debouncedQuery = useDebounce(localQuery)
-    const [list, setList] = useRecoilState(listState);
+  const history = useHistory();
+  const location = useLocation();
 
-    const { numberOfVideos, numberOfConferences } = useRecoilValue(computedResultDetails)
+  const match = useRouteMatch<{ name?: string }>(routeMatch ?? '');
 
-    // debounce
-    React.useEffect(() => {
-        if (debouncedQuery || debouncedQuery === '') {
-            setQuery(debouncedQuery)
-        }
-    }, [debouncedQuery]);
+  const [query, setQuery] = useRecoilState(queryState);
+  const [localQuery, setLocalQuery] = React.useState(query);
+  const debouncedQuery = useDebounce(localQuery);
+  const [list, setList] = useRecoilState(listState);
 
-    // set query state on load based off of url
-    React.useEffect(() => {
-        const search = queryString.parse(location.search);
-        if (search?.query && search.query !== '') {
-            setLocalQuery(search.query as string) // :/
-        }
-    }, [])
+  const { numberOfVideos, numberOfConferences } = useRecoilValue(
+    computedResultDetails
+  );
+  const [conference, setConference] = React.useState<
+    ConferenceTransformed | undefined
+  >(list?.[0]);
 
-    // set url based off query state 
-    React.useEffect(() => {
-        const url = query === '' ? location.pathname : `${location.pathname}?query=${sluggifyUrl(query)}`
-        history.push(encodeURI(url));
-    }, [query])
-
-    // update list based on search
-    React.useEffect(() => {
-            const getVideos = async () => {
-                if(!!query){
-                    const result = await search({query})
-                    setList(result)
-                }else{
-                    const data = await getList({start: 0})
-                    setList(data);
-                }
-            }
-            getVideos()
-    }, [query])
-
-    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLocalQuery(e.target.value)
+  // search over conference based off conference name
+  React.useEffect(() => {
+    const getVideos = async () => {
+      if (match?.params?.name) {
+        const result = await search({
+          conference: match.params.name,
+          query,
+        });
+        setList(result);
+      } else if (!!query) {
+        const result = await search({ query });
+        setList(result);
+      } else {
+        const data = await getList({ start: 0 });
+        setList(data);
+      }
     };
+    getVideos();
+  }, [match?.params?.name, query]);
 
-    return {
-        query,
-        localQuery,
-        list,
-        onInputChange,
-        numberOfVideos,
-        numberOfConferences
+  // debounce
+  React.useEffect(() => {
+    if (debouncedQuery || debouncedQuery === '') {
+      setQuery(debouncedQuery);
     }
-}
+  }, [debouncedQuery]);
 
-export default useSearch
+  // set conference details for conference/:name pages
+  React.useEffect(() => {
+    if (list.length) {
+      setConference(list[0]);
+    }
+  }, [list?.[0]?.title]);
+
+  // set query state on load based off of url
+  React.useEffect(() => {
+    const search = queryString.parse(location.search);
+    if (search?.query && search.query !== '') {
+      setLocalQuery(search.query as string); // :/
+    }
+  }, []);
+
+  // set url based off query state
+  React.useEffect(() => {
+    const url =
+      query === ''
+        ? location.pathname
+        : `${location.pathname}?query=${sluggifyUrl(query)}`;
+    history.push(encodeURI(url));
+  }, [query]);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalQuery(e.target.value);
+  };
+
+  return {
+    conference,
+    list,
+    localQuery,
+    numberOfConferences,
+    numberOfVideos,
+    onInputChange,
+    query,
+  };
+};
+
+export default useSearch;
